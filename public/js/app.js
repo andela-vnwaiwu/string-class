09f2174a58fba9fc98e0c4ed84121871983b0991
victor nwaiwu <victor.nwaiwu@andela.com>
2017-01-08 14:17:30 +0100

    feature(angular): add angular to the application

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+angular.module('StringExtension', [])
+.controller('StringExtensionController', ['$scope', '$timeout', ($scope, $timeout) => {
+  $scope.options = [
+    'hasVowels',
+    'toUpper',
+    'toLower',
+    'ucFirst',
+    'isQuestion',
+    'words',
+    'wordCount',
+    'toCurrency',
+    'fromCurrency',
+    'inverseCase',
+    'alternatingCase',
+    'getMiddle',
+    'numberWords',
+    'isDigit',
+    'doubleCheck'
+  ];
+
+  $scope.result = '';
+  $scope.error = '';
+  $scope.getResult = (input, selected) => {
+    if (input === '' || input === undefined){
+      $scope.result = '';
+      $scope.error = 'You did not enter any text';
+      return;
+    } else if (selected === undefined) {
+      $scope.result = '';
+      $scope.error = 'You did not select a method';
+      return;
+    }
+    $scope.error = '';
+    $scope.result = input[selected]();
+    return;
+  }
+}])
